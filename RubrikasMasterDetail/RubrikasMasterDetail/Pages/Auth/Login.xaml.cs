b24c70982141ddd76f7ff64ed0d3a0c025c914ff
William Llanos <williama.llanos@hotmail.com>
2017-11-16 16:25:39 -0500

    Testing AuthPages + ExtraAnnotations

---
+++ RubrikasMasterDetail/RubrikasMasterDetail/Pages/Auth/Login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RubrikasMasterDetail
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ConnectedContentPage
+	{
+		public Login (bool connected = true) : base(connected)
+		{
+			InitializeComponent ();
+		}
+
+		private async void OnSignUpButtonClicked(object sender, EventArgs e)
+		{
+			if (!IsConnected)
+			{
+                DisplayNotConnectedError();
+
+			}
+			else
+			{
+				
+			}
+		}
+
+		private async void OnLoginButtonClicked(object sender, EventArgs e)
+		{
+			if (!IsConnected)
+			{
+				DisplayNotConnectedError();
+
+			}
+			else
+			{
+
+			}
+		}
+	}
+}
